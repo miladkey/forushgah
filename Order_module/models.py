328634a38474fc98f17b24ee56a460c124cfd2ee
masoud <masood.keykha94@gmail.com>
2024-09-21 01:41:09 +0330

    change model

--- Order_module/models.py
+++ Order_module/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Order(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    paymentMethod = models.CharField(max_length=200, null=True, blank=True)
+    paymentMethod = models.CharField(max_length=300, null=True, blank=True)
     taxPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     totalPrice = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
@@ -14,7 +14,7 @@ class Order(models.Model):
     paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     isDelivered = models.BooleanField(default=False)
     deliveredAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
-    createdAt = models.DateTimeField(auto_now_add=True)
+    createdAt = models.DateTimeField(auto_now_add=True,)
 
 
 
